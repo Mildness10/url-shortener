fe81a32fad947aabd6f886d3634c3724047dbd72
Mildness10 <m.akomoize@gmail.com>
2023-08-17 19:37:54 -0400

    created the models for the app

--- url_app/models.py
+++ url_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ShortenURL(models.Model):
+    original_url = models.URLField()
+    short_key = models.CharField(max_length=10, unique=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank = True)
+    
+    def __str__(self):
+        return self.short_key

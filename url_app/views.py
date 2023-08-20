9780b5ea7b94371d53f6d4d03f206e90d614a187
Mildness10 <m.akomoize@gmail.com>
2023-08-19 23:59:27 -0400

    error mods

--- url_app/views.py
+++ url_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import ShortenURL
 
-# Create your views here.
+
+class HomeView(View):
+    template_name = 'home.html'
+    
+    def get(self, request):
+        shortened_urls = ShortenURL.objects.all()
+        return render(request, self.template_name, {'shortened_urls':shortened_urls})
+    
+    def post(self, request):
+        original_url = request.POST.get('original_url')
+        #code to generate the shortened URL using shrtco.de API
+        shortened_url = ShortenURL.objects.create(original_url=original_url, short_key="klm789")
+        shortened_url.generate_qr_code()
+        return redirect('home')
+        

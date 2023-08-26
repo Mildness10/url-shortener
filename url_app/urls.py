febdc76c247a26e964baca118b0271833185d45f
Mildness10 <m.akomoize@gmail.com>
2023-08-25 23:59:28 -0400

    defined urls for auth pages

--- url_app/urls.py
+++ url_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import HomeView
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('signup/', views.signup, name='signup'), 
 ]

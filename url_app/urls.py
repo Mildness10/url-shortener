eb44da756baf1871da729566cfc504602d8f44df
Mildness10 <m.akomoize@gmail.com>
2023-08-26 23:56:24 -0400

    created url route to handle user signup

--- url_app/urls.py
+++ url_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import HomeView
 from django.contrib.auth import views as auth_views
+from .views import SignUpView
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
-    path('signup/', views.signup, name='signup'), 
+    path('signup/', SignUpView.as_view(), name='signup'), 
 ]

e7269c84ece1bbeb596d7385ba64e6d330297b5a
Mildness10 <m.akomoize@gmail.com>
2023-09-12 23:55:19 -0400

    modified forms.py file

--- url_app/forms.py
+++ url_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
-
+#form to handle url submission
 class URLSubmissionForm(forms.Form):
     original_url = forms.URLField(label='Enter URL', required=True, widget=forms.URLInput(attrs={'class': 'form-control'}))
 
